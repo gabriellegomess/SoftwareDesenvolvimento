0f46fef1ce721e362407c7fe57b935bc28577fa1
Gabrielle Gomes <128157932+gabriellegomess@users.noreply.github.com>
2023-03-27 11:16:07 -0300

    Atualizacao
    Adc novas pastas

--- WorkTeste/ExeAva02/src/Programa.java
+++ WorkTeste/ExeAva02/src/Programa.java
@@ -1,10 +1,60 @@
+import java.util.Scanner;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
+		//ler dois vetores de 3 elementos 
+		//verificar se eles são iguais ou não
+		//para serem iguais todos os elementos devem coincidir 
+
+		Scanner leitor = new Scanner(System.in);
+
+		int[] vetorA = new int[3];
+		int[] vetorB = new int[3];
 		
+	for (int i = 0; i < vetorB.length; i++) {
 		
+		System.out.println("Digite os elementos do vetor A:");
+		vetorA[i] = leitor.nextInt();
+
 	}
 	
+	for (int i = 0; i < vetorB.length; i++) {
+	
+		System.out.println("Digite os elementoa do vetor B: ");
+		vetorB[i] = leitor.nextInt();
+	}
+	
+	int igual = 0;
+	int diferente = 0;
+	
+	for (int i = 0; i < vetorB.length; i++) {
+		
+		if(vetorA[i] == vetorB[i]) {
+			//System.out.println("Os vetores são iguais!");
+			igual++;
+		}else {
+			//System.out.println("Os vetores são diferentes");
+			diferente++;
+		}
+	}
+	
+	if(igual == 3){
+		System.out.println("Os vetores são iguais!");
+	}else {
+		System.out.println("Os vetores são diferentes!");
+	}
+	
+/*	for (int i = 0; i < vetorB.length; i++) {
+		//vetorA[i]  vetorB[i];
+		if(vetorA[i] != vetorB[i]) {
+			System.out.println("Os vetores são diferentes");
+		}
+	}*/
+	
+	
+	
+		leitor.close();
+	 }
 }
